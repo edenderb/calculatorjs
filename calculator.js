f0c1dd5fe5971058b68791bcc4e5c2f83040ad31
DevRavi <edenderb@gmail.com>
2022-03-02 10:46:07 +1100

    input functionality added

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-const currentOperand = document.getElementById("current-operand");
-const previousOperand = document.getElementById("previous-operand");
+const result = document.getElementById("current-operand");
+const last = document.getElementById("previous-operand");
 const numbers = document.querySelectorAll("[data-number]");
 const operator = document.getElementById("operator");
 const clear = document.getElementById("clear");
@@ -11,3 +11,60 @@ let currentOperator = null;
 let firstOperator = "";
 let secondOperator = "";
 let reset = false;
+
+//setting functionality of operators
+let add = (x, y) => {
+  answer = x + y;
+  currentOperator = null;
+  return answer;
+};
+
+let subtract = (x, y) => {
+  answer = x - y;
+  currentOperator = null;
+  return answer;
+};
+
+let multiply = (x, y) => {
+  answer = x * y;
+  currentOperator = null;
+  return answer;
+};
+
+let divide = (x, y) => {
+  answer = x / y;
+  currentOperator = null;
+  return answer;
+};
+
+function populateDisplay(number) {
+  if (reset.textContent === "0" || reset) {
+    resetScreen();
+  }
+  result.textContent += number;
+}
+
+function resetScreen() {
+  result.textContent = "";
+  reset = false;
+}
+
+function setOperation(operator) {
+  if (currentOperator != null) {
+    run();
+  }
+}
+
+function run() {
+  if (currentOperator === null || reset) {
+    return;
+  }
+  if (currentOperator === "%" && result.textContent === "0") {
+    result.textContent = "ERROR";
+    return;
+  }
+}
+
+numbers.forEach((button) =>
+  button.addEventListener("click", () => populateDisplay(button.textContent))
+);
