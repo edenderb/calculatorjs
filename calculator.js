e954ec45cce6e9a92a144befdda86fc8acea7d8e
DevRavi <edenderb@gmail.com>
2022-03-02 11:12:50 +1100

    All functionality of Calculator Completed

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 const result = document.getElementById("current-operand");
 const last = document.getElementById("previous-operand");
 const numbers = document.querySelectorAll("[data-number]");
-const operator = document.getElementById("operator");
+const operator = document.querySelectorAll("#operator");
 const clear = document.getElementById("clear");
 const del = document.getElementById("delete");
 const equals = document.getElementById("equals");
@@ -53,6 +53,10 @@ function setOperation(operator) {
   if (currentOperator != null) {
     run();
   }
+  firstOperator = result.textContent;
+  currentOperator = operator;
+  last.textContent = `${firstOperator} ${currentOperator}`;
+  reset = true;
 }
 
 function run() {
@@ -63,8 +67,55 @@ function run() {
     result.textContent = "ERROR";
     return;
   }
+
+  secondOperator = result.textContent;
+  last.textContent = `${firstOperator} ${currentOperator} ${secondOperator}`;
+  result.textContent = round(outcome(firstOperator, secondOperator));
+}
+function round(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function outcome(x, y) {
+  x = Number(x);
+  y = Number(y);
+  if (currentOperator === "+") {
+    return add(x, y);
+  } else if (currentOperator === "-") {
+    return subtract(x, y);
+  } else if (currentOperator === "*") {
+    return multiply(x, y);
+  } else if (currentOperator === "/") {
+    return divide(x, y);
+  }
+}
+
+function clearAll() {
+  result.textContent = "";
+  last.textContent = "";
+  currentOperator = null;
+  firstOperator = "";
+  secondOperator = "";
 }
 
 numbers.forEach((button) =>
   button.addEventListener("click", () => populateDisplay(button.textContent))
 );
+
+operator.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+equals.addEventListener("click", run);
+
+decimal.addEventListener("click", () => {
+  result.textContent += ".";
+  if (result.textContent.includes(".")) {
+    return;
+  }
+});
+
+clear.addEventListener("click", clearAll);
+
+del.addEventListener("click", () => {
+  result.textContent = result.textContent.toString().slice(0, -1);
+});
